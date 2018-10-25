b4856e2b37b78e6e4d8e1bd57d70d82a36c7368d
mac-186-71 <mac-186-71@mac-186-71.local>
2018-10-25 14:03:56 +0300

    Add 'react-native-geocoder' package.

--- app/utils/geolocation.js
+++ app/utils/geolocation.js
@@ -1,9 +1,13 @@
+import Geocoder from "react-native-geocoder";
+
+import { POLLUTION_DATA } from "@constants";
+
 export const getCurrentPosition = (options = {}) => {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject, options);
   });
 };
 
-export const getPollutionData = coords => {
+export const getPollutionData = ({ coords }) => {
   console.log(coords);
 };
